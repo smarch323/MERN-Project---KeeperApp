44549eb4f7e507d0ac539ab1ec6f93952018361e
smarch323 <smarch323@gmail.com>
2023-05-04 10:30:40 -0400

    yikes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.set('strictQuery', false);
 const connectDB = async () => {
   try{
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB Connected: ${conn.connection.host}');
+    console.log("MongoDB Connected: " + conn.connection.host);
   } catch(error){
     console.log(error);
     process.exit(1);
